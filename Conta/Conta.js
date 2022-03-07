b77fc9d05cc0d9d9940107edee0ab526cdac6609
unknown <adautodortafilho@gmail.com>
2022-03-07 00:47:59 -0300

    criação do sistema de autentificação

--- Conta/Conta.js
+++ Conta/Conta.js
@@ -2,7 +2,7 @@
 export class Conta {
     constructor(saldoInicial, cliente, agencia) {
         if(this.constructor == Conta){
-            throw new Error('Você não deveria instanciar um objeto do tipo Conta Diretamente');
+            throw new Error('Você não deveria instanciar um objeto do tipo Conta Diretamente, pois essa é uma classe abstrata');
         }
 
         this._saldo = saldoInicial;
@@ -26,12 +26,9 @@ export class Conta {
     }
 
 
-
+    // Método abstrato = feito para ser sobrescrito
     sacar(valor) {
-
-        let taxa = 1
-        return this._sacar(valor, taxa);
-
+        throw new Error("O método Sacar da conta é abstrato");
     }
 
     _sacar(valor, taxa) {
--- index.js
+++ index.js
@@ -1,14 +1,20 @@
-import { Cliente } from './Cliente.js'
-import { contaCorrente } from './ContaCorrente.js'
-import { ContaPoupanca } from './ContaPoupança.js';
-import { Conta } from './Conta.js';
+//import { Cliente } from './Conta/Cliente.js'
+import { Gerente } from './Funcionários/Gerente.js'
+import { Diretor } from './Funcionários/Diretor.js'
+import { SistemaAutenticacao } from './SistemaAutenticacao.js';
+import { Cliente } from './Cliente.js';
 
-const cliente1 = new Cliente('Ricardo', 11122233309);
+const diretor = new Diretor('Rodrigo', 10000, 12345678900);
+diretor.cadastrarSenha('123456');
+const gerente = new Gerente('Ricardo', 5000, 12378945601);
+gerente.cadastrarSenha('123');
 
-const contaCorrenteRicardo = new contaCorrente(cliente1, 1001);
-const contaPoupanca = new ContaPoupanca(50, cliente1, 1001);
-const conta = new Conta(0, cliente1, 1001);
+const cliente = new Cliente('Laís', 78945612379, '456');
+const gerenteEstaLogado = SistemaAutenticacao.login(gerente, '123');
+const diretorEstaLogado = SistemaAutenticacao.login(diretor, '123456');
 
-console.log(conta);
+const clienteEstaLogado = SistemaAutenticacao.login(cliente, '456');
+
+console.log(gerenteEstaLogado, diretorEstaLogado, clienteEstaLogado);
 
 
